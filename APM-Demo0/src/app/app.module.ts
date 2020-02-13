46148d97caa4afc2b52b935244fb3d34e9775400
Pato <pevargas2730@gmail.com>
2020-02-13 02:48:11 -0500

    added storemodule

--- APM-Demo0/src/app/app.module.ts
+++ APM-Demo0/src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 // Imports for loading & configuring the in-memory web api
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { ProductData } from "./products/product-data";
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { ShellComponent } from './home/shell.component';
-import { MenuComponent } from './home/menu.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { PageNotFoundComponent } from './home/page-not-found.component';
+import { AppComponent } from "./app.component";
+import { ShellComponent } from "./home/shell.component";
+import { MenuComponent } from "./home/menu.component";
+import { WelcomeComponent } from "./home/welcome.component";
+import { PageNotFoundComponent } from "./home/page-not-found.component";
 
 /* Feature Modules */
-import { UserModule } from './user/user.module';
+import { UserModule } from "./user/user.module";
+
+import { StoreModule } from "@ngrx/store";
 
 @NgModule({
   imports: [
@@ -23,7 +25,8 @@ import { UserModule } from './user/user.module';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(ProductData),
     UserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({})
   ],
   declarations: [
     AppComponent,
@@ -34,4 +37,4 @@ import { UserModule } from './user/user.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- APM-Demo0/src/app/products/product.module.ts
+++ APM-Demo0/src/app/products/product.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from "../shared/shared.module";
 
-import { ProductShellComponent } from './product-shell/product-shell.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductShellComponent } from "./product-shell/product-shell.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductEditComponent } from "./product-edit/product-edit.component";
 
-const productRoutes: Routes = [
-  { path: '', component: ProductShellComponent }
-];
+import { StoreModule } from "@ngrx/store";
+import { reducer } from "./state/product.reducer";
+
+const productRoutes: Routes = [{ path: "", component: ProductShellComponent }];
 
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule.forChild(productRoutes)
+    RouterModule.forChild(productRoutes),
+    StoreModule.forFeature("product", reducer)
   ],
   declarations: [
     ProductShellComponent,
@@ -22,4 +24,4 @@ const productRoutes: Routes = [
     ProductEditComponent
   ]
 })
-export class ProductModule { }
+export class ProductModule {}
