5ed4592223cd34cbd019e680a960ef9af8cfda86
Pato <pevargas2730@gmail.com>
2020-02-13 02:48:18 -0500

    created products reducer

---
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -0,0 +1,10 @@
+export function reducer(state, action) {
+  switch (action.type) {
+    case "TOGGLE_PRODUCT_CODE": {
+      return { ...state, showProductCode: action.payload };
+    }
+
+    default:
+      return state;
+  }
+}
