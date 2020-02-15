f781561434e8a6723edaf0754f026ff58b789879
Pato <pevargas2730@gmail.com>
2020-02-15 16:10:15 -0500

    Building selectors

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,5 +1,6 @@
 import { Product } from "../product";
 import * as fromRoot from "../../state/app.state";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 
 export interface State extends fromRoot.State {
   products: ProductState;
@@ -16,6 +17,25 @@ const initialState: ProductState = {
   currentProduct: null,
   products: []
 };
+
+/*Selector */
+
+const getProductFeatureState = createFeatureSelector<ProductState>("products");
+
+export const getShowProductCode = createSelector(
+  getProductFeatureState,
+  state => state.showProductCode //proyector function
+);
+
+export const getCurrentProduct = createSelector(
+  getProductFeatureState,
+  state => state.currentProduct //proyector function
+);
+
+export const getProducts = createSelector(
+  getProductFeatureState,
+  state => state.products //proyector function
+);
 
 export function reducer(state = initialState, action): ProductState {
   switch (action.type) {
