fb198a02dbe6f50ba184925b40e02e0f0fff37a8
Pato <pevargas2730@gmail.com>
2020-02-15 15:40:29 -0500

    solving issue with lazy loading

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,4 +1,9 @@
 import { Product } from "../product";
+import * as fromRoot from "../../state/app.state";
+
+export interface State extends fromRoot.State {
+  products: ProductState;
+}
 
 export interface ProductState {
   showProductCode: boolean;
--- APM-Demo0/src/app/state/app.state.ts
+++ APM-Demo0/src/app/state/app.state.ts
@@ -1,6 +1,3 @@
-import { ProductState } from "../products/state/product.reducer";
-
 export interface State {
-  products: ProductState;
-  user: any;
+  users: any;
 }
